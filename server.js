a54dd40bb4d49283100a37660853ddbd23452e10
Soita <soitabrian00@gmail.com>
2026-04-12 22:35:24 +0300

    Force rebuild

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 30001;
 
 // Middleware
 app.use(cors());
